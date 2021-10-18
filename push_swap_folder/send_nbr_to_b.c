14ab439cb77b3ecb5e9e554f262c09de92da47cd
artmende <artmende@student.s19.be>
2021-10-18 13:29:41 +0200

    put all to new norm and it works now

--- push_swap_folder/send_nbr_to_b.c
+++ push_swap_folder/send_nbr_to_b.c
@@ -6,7 +6,7 @@
 /*   By: artmende <artmende@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 14:36:10 by artmende          #+#    #+#             */
-/*   Updated: 2021/10/18 11:09:23 by artmende         ###   ########.fr       */
+/*   Updated: 2021/10/18 13:26:33 by artmende         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,15 @@ void	send_nbrs_under_pivot_value_to_b(int pivot_value, t_stacks_a_b *stacks)
 
 /*
 	choose_nbr_to_send :
-	browse all stack a and compare indexes, find the one closer
+
+	First find the closest number (from the top) that is in the pivot range
+	Then fast forward to the middle of the list
+	From there, for each number we meet that is in the pivot range, we compare
+	its index with the current nbr_to_send, if the index of the number we are
+	looking at is closer to the list edge, it become the new nbr_to_send.
+
+	The weird ++index followed by index-- was done to follow the norm of 42 and
+	keep the function at 25 lines max.
 */
 
 void	choose_nbr_to_send(int pivot_value, int size_a, t_stacks_a_b *stacks)
@@ -37,22 +45,23 @@ void	choose_nbr_to_send(int pivot_value, int size_a, t_stacks_a_b *stacks)
 
 	index = 0;
 	ptr = stacks->a;
-	while (ptr && index < size_a)
+	while (ptr && index < size_a && ++index)
 	{
-		if (ptr->nbr <= pivot_value)
+		if (ptr->nbr <= pivot_value && index--)
 		{
 			index_nbr_to_send = index;
 			break ;
 		}
-		(void)(++index && (ptr = ptr->next));
+		ptr = ptr->next;
 	}
-	while (ptr && index < size_a / 2)
-		(void)(++index && (ptr = ptr->next));
+	while (ptr && index < size_a / 2 && ++index)
+		ptr = ptr->next;
 	while (ptr)
 	{
 		if (ptr->nbr <= pivot_value && index_nbr_to_send > size_a - index)
 			index_nbr_to_send = index;
-		(void)(++index && (ptr = ptr->next));
+		index++;
+		ptr = ptr->next;
 	}
 	send_nbr_to_b(size_a, index_nbr_to_send, stacks);
 }
